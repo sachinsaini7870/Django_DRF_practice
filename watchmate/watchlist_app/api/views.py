50e3333af55d287679e7b8361d99fc6b28ce1495
SACHIN <sachinsaini7870@gmail.com>
2026-01-01 18:50:55 +0530

    create crud api function based view with status code using drf

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,17 +1,52 @@
 from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.response import Response
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def movie_list(request):
-    movie = Movie.objects.all()
-    serializer = MovieSerializer(movie, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        movie = Movie.objects.all()
+        serializer = MovieSerializer(movie, many=True)
+        return Response(serializer.data)
 
-@api_view()
+    if request.method == "POST":
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
 def movie_details(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    if request.method == "GET":
+        
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response(
+                {"Error": "Movie not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+            
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    if request.method == "PUT":
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == "DELETE":
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie)
+        movie.delete()
+        content = {"message": f"'{serializer.data.get('name')}' deleted successfully."}
+        return Response(content, status=status.HTTP_204_NO_CONTENT)
