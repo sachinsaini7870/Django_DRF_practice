34d658ddb518b23bc66ea1bbf503d430e9f02f5d
SACHIN <sachinsaini7870@gmail.com>
2026-01-01 22:19:20 +0530

    create crud api class based view with status code using drf

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
-from watchlist_app.api.views import movie_list, movie_details
+
+# from watchlist_app.api.views import movie_list, movie_details
+from watchlist_app.api.views import MovieListAV, MovieDetailsAV
 
 urlpatterns = [
-    path('list/', movie_list, name='movie-list'),
-    path('<int:pk>', movie_details, name='movie-details'),
+    # path('list/', movie_list, name='movie-list'),
+    # path('<int:pk>', movie_details, name='movie-details'),
+    path("list/", MovieListAV.as_view(), name="movie-list"),
+    path("<int:pk>", MovieDetailsAV.as_view(), name="movie-details"),
 ]
