719de3eb567fdf745d1b1c6aba75b6d7cbcd0c15
SACHIN <sachinsaini7870@gmail.com>
2026-01-02 13:32:17 +0530

    Add one to one relationship between StreamingPlatform and Watchlist

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
 # from watchlist_app.api.views import movie_list, movie_details
-from watchlist_app.api.views import WatchListAV, WatchDetailsAV, StreamingPlatformAV
+from watchlist_app.api.views import WatchListAV, WatchDetailsAV, StreamingPlatformAV, StreamPlatformDetailAV
+
 
 
 
@@ -9,5 +10,6 @@ urlpatterns = [
     path("list/", WatchListAV.as_view(), name="movie-list"),
     path("<int:pk>", WatchDetailsAV.as_view(), name="movie-details"),
     path("stream/", StreamingPlatformAV.as_view(), name="stream"),
+    path("stream/<int:pk>", StreamPlatformDetailAV.as_view(), name="stream-details"),
 
 ]
--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -24,6 +24,33 @@ class StreamingPlatformAV(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class StreamPlatformDetailAV(APIView):
+    def get(self, request, pk):
+        try:
+            platform = StreamingPlatform.objects.get(pk=pk)
+        except StreamingPlatform.DoesNotExist:
+            return Response(
+                {"Error": "Platform not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = StreamingPlatformSerializer(platform)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        platform = StreamingPlatform.objects.get(pk=pk)
+        serializer = StreamingPlatformSerializer(platform, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        platform = StreamingPlatform.objects.get(pk=pk)
+        serializer = StreamingPlatformSerializer(platform)
+        platform.delete()
+        content = {"message": f"'{serializer.data.get('name')}' deleted successfully."}
+        return Response(content, status=status.HTTP_204_NO_CONTENT)
 
 class WatchListAV(APIView):
 
