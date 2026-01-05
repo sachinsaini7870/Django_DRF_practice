0b8d235f46f2472e9b7dcc1b6167182cecba90af
SACHIN <sachinsaini7870@gmail.com>
2026-01-05 12:40:49 +0530

    Create logout api for delete token from db

--- watchmate/user_app/api/views.py
+++ watchmate/user_app/api/views.py
@@ -1,9 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework import status
 
 from user_app.api.serializers import RegistrationSerializer
 
+@api_view(['POST'])
+def logout_view(request):
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
 
 @api_view(["POST"])
 def registration_view(request):
@@ -21,4 +27,4 @@ def registration_view(request):
         else:
             data = serializer.errors
 
-        return Response(data, status=201)
+        return Response(data, status=status.HTTP_201_CREATED)
