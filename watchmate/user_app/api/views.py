102e5c3a2e7e8a0b0a1083307cf95ea3b28c8fa6
SACHIN <sachinsaini7870@gmail.com>
2026-01-05 12:29:49 +0530

    Add auto generate token for new registration by model using signals

--- watchmate/user_app/api/views.py
+++ watchmate/user_app/api/views.py
@@ -1,14 +1,24 @@
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
 from user_app.api.serializers import RegistrationSerializer
-from rest_framework.response import Response
 
 
-@api_view(["POST", "GET"])
+@api_view(["POST"])
 def registration_view(request):
     if request.method == "POST":
         serializer = RegistrationSerializer(data=request.data)
+        data = {}
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+            account = serializer.save()
+
+            data["response"] = "Registration Successful!"
+            data["username"] = account.username
+            data["email"] = account.email
+            token = Token.objects.get(user=account).key
+            data["token"] = token
+        else:
+            data = serializer.errors
+
+        return Response(data, status=201)
