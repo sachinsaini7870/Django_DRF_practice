9e5358c9f61a239135272135c5213d74d9672311
SACHIN <sachinsaini7870@gmail.com>
2026-01-05 16:14:44 +0530

    Add admin access permision to reviews and add review user as str in review model

--- watchmate/watchlist_app/api/permissions.py
+++ watchmate/watchlist_app/api/permissions.py
@@ -12,4 +12,4 @@ class IsReviewUserOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         else:
-            return obj.review_user == request.user
+            return obj.review_user == request.user or request.user.is_staff
