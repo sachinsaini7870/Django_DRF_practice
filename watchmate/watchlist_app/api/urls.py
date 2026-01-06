448fc2c963fcc0c93698d0b55802f1f89f640571
SACHIN <sachinsaini7870@gmail.com>
2026-01-06 22:17:22 +0530

    Add Streaming Platform Test cases

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -21,18 +21,14 @@ router.register("stream", StreamingPlatformVS, basename="streamingplatform")
 urlpatterns = [
     path("list/", WatchListAV.as_view(), name="movie-list"),
     path("<int:pk>/", WatchDetailsAV.as_view(), name="movie-detail"),
-    path("list2/", WatchListGV.as_view(), name="watch-list"),     # Only for test purpose
-
+    path("list2/", WatchListGV.as_view(), name="watch-list"),  # Only for test purpose
     path("", include(router.urls)),
-
     # path("stream/", StreamingPlatformAV.as_view(), name="stream-list"),
     # path("stream/<int:pk>", StreamingPlatformDetailAV.as_view(), name="streamingplatform-detail"),
-    
     # path("review/", ReviewList.as_view(), name="review-list"),
     # path("review/<int:pk>", ReviewDetail.as_view(), name="review-detail"),
-    
-    path("<int:pk>/review-create/", ReviewCreate.as_view(), name="review-create"),    
-    path("<int:pk>/reviews/", ReviewList.as_view(), name="review-list"),    
+    path("<int:pk>/review-create/", ReviewCreate.as_view(), name="review-create"),
+    path("<int:pk>/reviews/", ReviewList.as_view(), name="review-list"),
     path("review/<int:pk>/", ReviewDetail.as_view(), name="review-detail"),
     # path("reviews/<str:username>/", UserReview.as_view(), name="user-review-list"),
     path("reviews/", UserReview.as_view(), name="user-review-list"),
