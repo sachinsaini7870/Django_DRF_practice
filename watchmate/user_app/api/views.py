f040e6b3000b6d57bbed90308a2214793b955c11
SACHIN <sachinsaini7870@gmail.com>
2026-01-06 10:24:17 +0530

    get jwt token on registration

--- watchmate/user_app/api/views.py
+++ watchmate/user_app/api/views.py
@@ -2,14 +2,17 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from user_app.api.serializers import RegistrationSerializer
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 def logout_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         request.user.auth_token.delete()
         return Response(status=status.HTTP_200_OK)
+
 
 @api_view(["POST"])
 def registration_view(request):
@@ -22,8 +25,16 @@ def registration_view(request):
             data["response"] = "Registration Successful!"
             data["username"] = account.username
             data["email"] = account.email
-            token = Token.objects.get(user=account).key
-            data["token"] = token
+
+            # token = Token.objects.get(user=account).key
+            # data["token"] = token
+
+            refresh = RefreshToken.for_user(account)
+            data["token"] = {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }
+
         else:
             data = serializer.errors
 
