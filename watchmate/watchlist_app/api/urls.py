b43c2adc5dcfe14fc90ad456202ea6535b167488
SACHIN <sachinsaini7870@gmail.com>
2026-01-06 12:45:10 +0530

    Learn filter with query params

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -32,5 +32,6 @@ urlpatterns = [
     path("<int:pk>/review-create/", ReviewCreate.as_view(), name="review-create"),    
     path("<int:pk>/reviews/", ReviewList.as_view(), name="review-list"),    
     path("review/<int:pk>/", ReviewDetail.as_view(), name="review-detail"),
-    path("reviews/<str:username>/", UserReview.as_view(), name="user-review-list"),
+    # path("reviews/<str:username>/", UserReview.as_view(), name="user-review-list"),
+    path("reviews/", UserReview.as_view(), name="user-review-list"),
 ]
--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -24,8 +24,12 @@ class UserReview(generics.ListAPIView):
     serializer_class = ReviewSerializer
     # throttle_classes = [ReviewListThrottle, AnonRateThrottle]
 
+    # def get_queryset(self):
+    #     username = self.kwargs['username']
+    #     return Review.objects.filter(review_user__username=username)
+
     def get_queryset(self):
-        username = self.kwargs['username']
+        username = self.request.query_params.get('username')
         return Review.objects.filter(review_user__username=username)
 
 class ReviewCreate(generics.CreateAPIView):
